a2bebe69bb19235e4c097471f55f6c6e10811178
wdenberg <wdenbergskillshd@hotmail.com>
2024-10-25 15:13:32 -0300

    feat: Service layer, component registration

--- src/main/java/com/wrdeveloper/wrapi/resourcers/UserResource.java
+++ src/main/java/com/wrdeveloper/wrapi/resourcers/UserResource.java
@@ -2,19 +2,36 @@ package com.wrdeveloper.wrapi.resourcers;
 
 import com.wrdeveloper.wrapi.entities.User;
 
+import com.wrdeveloper.wrapi.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping( value= "/user")
 public class UserResource {
 
 
+   @Autowired
+   private UserService service;
+
+
    @GetMapping
-   public ResponseEntity<User> findAll(){
-      User us = new User(1L, "Wdenberg", "wdenberg@gmail.com", "123456", "99999999");
-      return ResponseEntity.ok().body(us);
+   public ResponseEntity<List<User>> findAll(){
+      List<User> list = service.findAll();
+      return ResponseEntity.ok().body(list);
    }
+
+   @GetMapping(value = "/{id}")
+   public  ResponseEntity<User> findByID(@PathVariable Long  id){
+      User obj = service.findById(id);
+      return ResponseEntity.ok().body(obj);
+   }
+
+
 }

82f5e1c7c2d5a6945a6acffbac2f501071bfb580
wdenberg <wdenbergskillshd@hotmail.com>
2024-10-28 19:26:40 -0300

    feat: OrderStatus enum

--- src/main/java/com/wrdeveloper/wrapi/config/TestConfig.java
+++ src/main/java/com/wrdeveloper/wrapi/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.wrdeveloper.wrapi.config;
 
 import com.wrdeveloper.wrapi.entities.Order;
 import com.wrdeveloper.wrapi.entities.User;
+import com.wrdeveloper.wrapi.entities.enus.OrderStatus;
 import com.wrdeveloper.wrapi.repositores.OrderRepository;
 import com.wrdeveloper.wrapi.repositores.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,9 +28,9 @@ public class TestConfig implements CommandLineRunner {
         User us1 = new User(null, "Berg Ramos", "berg@gmail.com", "123456", "8888888");
 
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), us1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), us);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), us1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, us1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, us);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT,us1);
         userRepository.saveAll(Arrays.asList(us, us1));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
     }
